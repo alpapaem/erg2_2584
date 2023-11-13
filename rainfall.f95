16c67c8bd27f04665ba7cdf8d43198227e694197
alpapaem <alex.papaem@gmail.com>
2023-11-13 23:28:09 +0200

    c2 - Read and display 5 items from data.txt

--- rainfall.f95
+++ rainfall.f95
@@ -1,5 +1,26 @@
 program rainfall
+  implicit none
+  integer :: i
+  character(20),dimension(5) :: town
+  integer,dimension(5):: rain
+
+  ! Open file
+  open(unit=10, file="data.txt", status='old', action='read')
 
 
+  !read Header
+  read(10, *)  
+
+  !read data
+  do i = 1, 5
+    read(10, *) town(i), rain(i)
+  end do
+
+  !print data
+  do i = 1, 5
+    write(*,*) town(i), rain(i)
+  end do
+
+  close(10)
 
 end program
