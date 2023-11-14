dea6886871ef0faec60a9a43bbd05b72f27627ad
alpapaem <alex.papaem@gmail.com>
2023-11-14 18:58:00 +0200

    c5 - Created subroutine to read file

--- rainfall.f95
+++ rainfall.f95
@@ -1,42 +1,59 @@
 program Rainfall
   implicit none
-  integer :: i,Nmax,n,iostat
+  integer :: i,Nmax,rows,iostat,N
   character(100)::line
-  character(20),dimension(50):: town
-  integer,dimension(50):: rain
+  character(20),dimension(:),allocatable:: T
+  integer,dimension(:),allocatable:: R
 
   ! Open file
   open(unit=10, file="data.txt", status='old', action='read')
-    
-  !read number of rows in file and rewind to the top
-  Nmax=50
-  n = 0
+ 
+  !read the number of rows inside the file and at the end rewind to the top
+  rows = 0
   do
     read(10, *, iostat=iostat) line
     if (iostat /= 0) exit  
-    n = n + 1
+    rows = rows + 1
   end do
   rewind(10)
 
-   !read data and place into the arrays if number of rows is bellow 50
-   if (n<=Nmax) then   
-     read(10, *)
-        
-     do i = 1, n-1
-       read(10, *) town(i), rain(i)
-     end do
+  !ask for the Nmax
+  write(*,*) 'Give Max number of Rows (Nmax) you want without the Header'
+  read(*,*) Nmax
 
-     do i = 1, n-1
-       write(*,*) town(i), rain(i)
+  !check the requested size of Nmax
+  If (Nmax<=rows) then
+     allocate (T(Nmax))                        !allocate Arrays
+     allocate (R(Nmax))
+     
+     call FillArrays(Nmax,T,R)                   !call subroutine 
+ 
+     do i = 1, Nmax                              !print results
+       write(*,*) T(i), R(i)
      end do
-  end if
-  
-  if (n>Nmax) then
-     print*,'Error: Number of Rows in file need to be at most 50'
+     write(*,*) 'The number of items in each Array is',N
   end if
 
-  
+  If (Nmax>rows) then
+    write(*,*) 'Error: Nmax is bigger than the number of rows in the data.txt file'
+  end if
 
  close(10)
  
+ contains                                  !subroutine
+ 
+ subroutine FillArrays(Nmax,T,R)
+     integer::Nmax,i
+     character(20),dimension(Nmax):: T
+     integer,dimension(Nmax):: R
+     
+     read(10, *)
+     N=0
+     do i = 1, Nmax
+       read(10, *) T(i), R(i)
+       N=N+1
+     end do
+ end subroutine FillArrays
+
+
 end program Rainfall
