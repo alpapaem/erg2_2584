de93e6eab682e6505317fa6e5df4913539e2d3fe
alpapaem <alex.papaem@gmail.com>
2023-11-14 17:29:32 +0200

    c4 - Read and display all items (up to 50) from data.txt

--- rainfall.f95
+++ rainfall.f95
@@ -1,26 +1,42 @@
-program rainfall
+program Rainfall
   implicit none
-  integer :: i
-  character(20),dimension(5) :: town
-  integer,dimension(5):: rain
+  integer :: i,Nmax,n,iostat
+  character(100)::line
+  character(20),dimension(50):: town
+  integer,dimension(50):: rain
 
   ! Open file
   open(unit=10, file="data.txt", status='old', action='read')
-
-
-  !read Header
-  read(10, *)  
-
-  !read data
-  do i = 1, 5
-    read(10, *) town(i), rain(i)
+    
+  !read number of rows in file and rewind to the top
+  Nmax=50
+  n = 0
+  do
+    read(10, *, iostat=iostat) line
+    if (iostat /= 0) exit  
+    n = n + 1
   end do
+  rewind(10)
 
-  !print data
-  do i = 1, 5
-    write(*,*) town(i), rain(i)
-  end do
+   !read data and place into the arrays if number of rows is bellow 50
+   if (n<=Nmax) then   
+     read(10, *)
+        
+     do i = 1, n-1
+       read(10, *) town(i), rain(i)
+     end do
 
-  close(10)
+     do i = 1, n-1
+       write(*,*) town(i), rain(i)
+     end do
+  end if
+  
+  if (n>Nmax) then
+     print*,'Error: Number of Rows in file need to be at most 50'
+  end if
 
-end program
+  
+
+ close(10)
+ 
+end program Rainfall
